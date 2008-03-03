82c0962bd1adf40681839aed660896d3a19c912f
mtdarby <mtdarby@8678a7ab-9d45-0410-84f9-79204fc2876f>
2008-03-03 01:15:11 +0000

    Added support for non-CRUD methods
    git-svn-id: http://restful-acl.googlecode.com/svn/tags/restful_acl@10 8678a7ab-9d45-0410-84f9-79204fc2876f

--- lib/restful_acl.rb
+++ lib/restful_acl.rb
@@ -1,5 +1,3 @@
-# RestfulAcl
-
 module RestfulAcl
   
   def self.included(base)
@@ -10,31 +8,29 @@ module RestfulAcl
   module ClassMethods
     
     def has_permission?
-      begin
-        # Load the Model based on the controller name passed in
-        klass = Object.const_get(params[:controller].classify)
-    
-        # Load the object requested if the param[:id] exists
-        object = klass.find(params[:id]) unless params[:id].blank?
-       
-        # Let's let the Model decide what is acceptable
-        access_granted = case params[:action]
-          when "index":   klass.is_readable_by(current_user)
-          when "show":    klass.is_readable_by(current_user, object)
-          when "edit":    object.is_updatable_by(current_user)
-          when "update":  object.is_updatable_by(current_user)
-          when "new":     klass.is_creatable_by(current_user)
-          when "create":  klass.is_creatable_by(current_user)
-          when "destroy": object.is_deletable_by(current_user)
-        end   
-
-        permission_denied unless access_granted
-    
-      rescue
-        # Failsafe: If any funny business is going on, log and redirect
-        routing_error_occurred
+      # Load the Model based on the controller name passed in
+      klass = Object.const_get(params[:controller].classify)
+      
+      # Load the object requested if the param[:id] exists
+      object = klass.find(params[:id]) unless params[:id].blank?
+      
+      # Let's let the Model decide what is acceptable
+      permission_denied unless case params[:action] 
+        when "index"          then klass.is_readable_by(current_user)
+        when "show"           then klass.is_readable_by(current_user, object)
+        when "edit", "update" then object.is_updatable_by(current_user)
+        when "new", "create"  then klass.is_creatable_by(current_user)
+        when "destroy"        then object.is_deletable_by(current_user)
+        else klass.is_readable_by(current_user)
       end
+      
+    rescue
+      # Failsafe: If any funny business is going on, log and redirect
+      routing_error
     end
+
+
+    private
 
     def permission_denied
       logger.info("[ACL] Permission denied to %s at %s for %s" %
@@ -43,11 +39,12 @@ module RestfulAcl
       redirect_to denied_url
     end
 
-    def routing_error_occurred
+    def routing_error
       logger.info("[ACL] Routing error by %s at %s for %s" %
       [(logged_in? ? current_user.login : 'guest'), Time.now, request.request_uri])
   
       redirect_to error_url
     end
+  
   end
 end
