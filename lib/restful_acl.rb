43e91142f4a0a1747d9cd15217582ebb8e503b08
mtdarby <mtdarby@8678a7ab-9d45-0410-84f9-79204fc2876f>
2008-03-03 12:14:31 +0000

    Corrected begin/rescue
    git-svn-id: http://restful-acl.googlecode.com/svn/tags/restful_acl@12 8678a7ab-9d45-0410-84f9-79204fc2876f

--- lib/restful_acl.rb
+++ lib/restful_acl.rb
@@ -8,25 +8,27 @@ module RestfulAcl
   module ClassMethods
     
     def has_permission?
-      # Load the Model based on the controller name passed in
-      klass = Object.const_get(params[:controller].classify)
+      begin
+        # Load the Model based on the controller name passed in
+        klass = Object.const_get(params[:controller].classify)
       
-      # Load the object requested if the param[:id] exists
-      object = klass.find(params[:id]) unless params[:id].blank?
+        # Load the object requested if the param[:id] exists
+        object = klass.find(params[:id]) unless params[:id].blank?
       
-      # Let's let the Model decide what is acceptable
-      permission_denied unless case params[:action] 
-        when "index"          then klass.is_readable_by(current_user)
-        when "show"           then klass.is_readable_by(current_user, object)
-        when "edit", "update" then object.is_updatable_by(current_user)
-        when "new", "create"  then klass.is_creatable_by(current_user)
-        when "destroy"        then object.is_deletable_by(current_user)
-        else klass.is_readable_by(current_user)
+        # Let's let the Model decide what is acceptable
+        permission_denied unless case params[:action] 
+          when "index"          then klass.is_readable_by(current_user)
+          when "show"           then klass.is_readable_by(current_user, object)
+          when "edit", "update" then object.is_updatable_by(current_user)
+          when "new", "create"  then klass.is_creatable_by(current_user)
+          when "destroy"        then object.is_deletable_by(current_user)
+          else klass.is_readable_by(current_user)
+        end
+      
+      rescue
+        # Failsafe: If any funny business is going on, log and redirect
+        routing_error
       end
-      
-    rescue
-      # Failsafe: If any funny business is going on, log and redirect
-      routing_error
     end
 
 
