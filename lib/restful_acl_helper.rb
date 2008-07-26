029e6406e3de259c62310a96746030e9087b1d49
Matt Darby <matt@matt-darby.com>
2008-07-26 15:17:42 -0400

    Aliased methods for the spelling-challenged

--- lib/restful_acl_helper.rb
+++ lib/restful_acl_helper.rb
@@ -5,25 +5,30 @@ module RestfulAclHelper
     
     klass.is_creatable_by(@current_user)
   end
-
+  alias_method :createable, :creatable
+  
+  
   def updatable(object)
     return true if admin_enabled
     
     object.is_updatable_by(@current_user)
   end
-
+  alias_method :updateable, :updatable
+  
+  
   def deletable(object)
     return true if admin_enabled
     
     object.is_deletable_by(@current_user)
   end
-
+  alias_method :deleteable, :deletable
+  
+  
   def readable(object = nil)
     return true if admin_enabled
     
     klass.is_readable_by(@current_user, object)
-  end
-
+  end  
 
   private
     
