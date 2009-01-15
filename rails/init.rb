236ea9f435b9e3f980a290ef78789e08aec2e3a1
Matt Darby <matt@matt-darby.com>
2009-01-15 14:04:26 -0500

    Added RestfulAclModel module

--- rails/init.rb
+++ rails/init.rb
@@ -1,7 +1,9 @@
 require 'restful_acl'
 require 'restful_acl_helper'
+require 'restful_acl_model'
 
 ActionController::Base.send :include, RestfulAcl
 ActionView::Base.send :include, RestfulAclHelper
+ActiveRecord::Base.send :include, RestfulAclModel
 
 RAILS_DEFAULT_LOGGER.debug "** [RESTful_ACL] loaded"
