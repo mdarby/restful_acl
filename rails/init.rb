960e95f2d2dca8c156f60a126d78f4aefdb68bde
Matt Darby <matt@matt-darby.com>
2009-01-15 14:31:14 -0500

    Updated for renamed lib

--- rails/init.rb
+++ rails/init.rb
@@ -1,8 +1,8 @@
-require 'restful_acl'
+require 'restful_acl_controller'
 require 'restful_acl_helper'
 require 'restful_acl_model'
 
-ActionController::Base.send :include, RestfulAcl
+ActionController::Base.send :include, RestfulAclController
 ActionView::Base.send :include, RestfulAclHelper
 ActiveRecord::Base.send :include, RestfulAclModel
 
