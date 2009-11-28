404a28dfe087336dd4fc1dcd3a9ce1735d6ed9bd
Matt Darby <matt@matt-darby.com>
2009-11-28 13:28:01 -0500

    requiring new libs

--- lib/restful_acl.rb
+++ lib/restful_acl.rb
@@ -1,3 +1,6 @@
+require 'restful_acl/string'
+require 'restful_acl/url_parser'
+require 'restful_acl/base'
 require 'restful_acl/controller'
 require 'restful_acl/helper'
 require 'restful_acl/model'
