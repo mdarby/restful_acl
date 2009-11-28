51a7739f2be7d345e818738a6e11d58ef007fb64
Matt Darby <matt@matt-darby.com>
2009-11-28 13:31:36 -0500

    Added spec helper file for rspec tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'restful_acl'
+require 'spec'
+require 'spec/autorun'
+require 'rubygems'
+require 'activesupport'
+
+Spec::Runner.configure do |config|
+  
+end
