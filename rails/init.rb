a70131837591ec7300248186a74d40997140e15d
Matt Darby <matt@matt-darby.com>
2009-11-28 13:29:26 -0500

    Use new namespaced modules

--- rails/init.rb
+++ rails/init.rb
@@ -1,7 +1,7 @@
 require 'restful_acl'
 
-ActionController::Base.send :include, RestfulAclController
-ActionView::Base.send :include, RestfulAclHelper
-ActiveRecord::Base.send :include, RestfulAclModel
+ActionController::Base.send :include, RestfulAcl::Controller
+ActionView::Base.send :include, RestfulAcl::Helper
+ActiveRecord::Base.send :include, RestfulAcl::Model
 
 RAILS_DEFAULT_LOGGER.debug "** [RESTful_ACL] loaded"
