8d095678d7939ca57e19e30a272f2a3b6881bdec
Matt Darby <matt@matt-darby.com>
2009-12-02 14:11:32 -0500

    Added obsolete methods error message

--- lib/restful_acl/helper.rb
+++ lib/restful_acl/helper.rb
@@ -8,5 +8,17 @@ module RestfulAcl
       yield if access.allowed?
     end
 
+    %w{creat delet updat read}.each do |m|
+      define_method("#{m}able?") do |*args, &block|
+        RAILS_DEFAULT_LOGGER.error <<-STR
+
+          *****************************************************************************************
+          * RESTful_ACL error => ##{m}able? has been removed as of v3.0.0, please use #allowable?
+          *****************************************************************************************
+
+        STR
+      end
+    end
+
   end
 end
