53063097ff6802f8d97534107bf3e6b85612a678
Matt Darby <matt@matt-darby.com>
2010-08-31 14:55:03 -0400

    Fixed Railtie loading in Rails3
    I was testing this a plugin previously, hence the Railtie issue.

--- lib/restful_acl.rb
+++ lib/restful_acl.rb
@@ -1,3 +1,4 @@
+require 'restful_acl/railtie' if defined?(Rails)
 require 'restful_acl/string'
 require 'restful_acl/url_parser'
 require 'restful_acl/base'
--- lib/restful_acl/railtie.rb
+++ lib/restful_acl/railtie.rb
@@ -1,19 +1,33 @@
+# require 'restful_acl'
+# 
+# module RestfulAcl
+#   if defined? Rails::Railtie
+#     require 'rails'
+#     class Railtie < Rails::Railtie
+#       initializer 'restful_acl.insert_into_active_record' do
+#         ActiveSupport.on_load :active_record do
+#           RestfulAcl::Railtie.insert
+#         end
+#       end
+#     end
+#   end
+# 
+#   class Railtie
+#     def self.insert
+#       ActiveRecord::Base.send(:include, RestfulAcl::Model)
+#       ActionController::Base.send(:include, RestfulAcl::Controller)
+#       ActionView::Base.send(:include, RestfulAcl::Helper)
+#     end
+#   end
+# end
+
+
 require 'restful_acl'
+require 'rails'
 
 module RestfulAcl
-  if defined? Rails::Railtie
-    require 'rails'
-    class Railtie < Rails::Railtie
-      initializer 'restful_acl.insert_into_active_record' do
-        ActiveSupport.on_load :active_record do
-          RestfulAcl::Railtie.insert
-        end
-      end
-    end
-  end
-
-  class Railtie
-    def self.insert
+  class Railtie < Rails::Railtie
+    initializer 'restful_acl.insert' do
       ActiveRecord::Base.send(:include, RestfulAcl::Model)
       ActionController::Base.send(:include, RestfulAcl::Controller)
       ActionView::Base.send(:include, RestfulAcl::Helper)
