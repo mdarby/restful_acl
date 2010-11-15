95d534276c9263c130729b1bbca2949d64334ace
Matt Darby <matt@matt-darby.com>
2010-11-15 12:54:42 -0500

    Don't be greedy and use new Rails3 UJS standards

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,18 +9,15 @@ require 'activesupport'
 Spec::Runner.configure do |config|
 
   def url(path)
-    "<a href=\"#{path}\">#{path}</a>"
+    "<a href=\"#{path}\" target=\"_blank\" alt=\"foo\">#{path}</a>"
   end
 
   def destroy_url(path)
-    "<a href=\"#{path}\" onclick=\"if (confirm('Are you sure?')) { var f = document.createElement('form');
-     f.style.display = 'none'; this.parentNode.appendChild(f); f.method = 'POST'; f.action = this.href;
-     var m = document.createElement('input'); m.setAttribute('type', 'hidden'); m.setAttribute('name', '_method');
-     m.setAttribute('value', 'delete'); f.appendChild(m);f.submit(); };return false;\">Delete Image</a>"
+    "<a href=\"#{path}\" data-method=\"delete\">Delete Image</a>"
   end
 
   def ajax_url(path)
-    "<a href=\"#\" onclick=\"if (confirm('Are you sure?')) { $.ajax({beforeSend:function(request){;}, complete:function(request){}, data:'_method=delete', dataType:'script', type:'post', url:'#{path}'}); }; return false;\"></a>"
+    "<a href=\"#{path}\" data-remote=\"true\">AJAX</a>"
   end
 
 end
